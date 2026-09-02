a942aeb39a5e4d9e8946bbe4cf08a874a4effef9
Riski1616 <wwa725430@gmail.com>
2026-09-03 06:52:34 +0800

    Create Day02.java

---
+++ Day02.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Menggunakan println()
+        System.out.println("BIODATA DIRI");
+        System.out.println("Nama        =    Riski");
+        System.out.println("NIM         =    D0226512");
+        System.out.println("Kelas       =    B");
+
+        // Menggunakan print()
+        System.out.print("Prodi       =    ");
+        System.out.print("Informatika");
+        System.out.print("\n");
+        
+        // Menggunakan printf()
+        String kota = "Kabupaten Pasangkayu";
+        int umur = 18;
+
+        System.out.printf("Kota        =    %s%n", kota);
+        System.out.printf("Umur        =    %d tahun%n", umur);
+    }
+}
