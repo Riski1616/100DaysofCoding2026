091f54449a17bf7042fc169f21e4eccb25163031
Riski1616 <wwa725430@gmail.com>
2026-09-02 21:03:47 +0800

    Create Day01.java

---
+++ Day01.java
@@ -0,0 +1,17 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		// Metode utama dalam Java untuk menjalankan program dalam satu baris
+		System.out.println("Hello World");
+		
+		/* Digunakan untuk menulis komentar
+           dalam beberapa baris  
+           Nama = Riski
+           Kelas = B
+           Materi = java */
+           
+		/*** Digunakan untuk memberikan dokumentasi pada kode ***/
+		System.out.println("Riski");
+		
+	}
+}
