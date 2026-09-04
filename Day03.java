47840ae1cfafd0823fb9ae9d059df197f346a329
Riski1616 <wwa725430@gmail.com>
2026-09-04 19:41:50 +0800

    Update Day03.java

--- Day03.java
+++ Day03.java
@@ -1,21 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Escape Sequence \n
+        // Escape Sequence \n = membuat baris baru
         System.out.println("Nama         : Riski\nKelas        : B");
 
-        // Escape Sequence \t
+        // Escape Sequence \t = membuat jarak/tab
         System.out.println("Nim\t     : D0226512");
         System.out.println("Prodi\t     : Informatika");
-        System.out.println("alamat\t     : Majene");
+        System.out.println("Alamat\t     : Majene");
 
-        // Escape Sequence \"
+        // Escape Sequence \" = menampilkan tanda kutip
         System.out.println("Saya berkata : \"Saya sedang belajar Java\"");
 
-        // Escape Sequence \\
+        // Escape Sequence \\ = menampilkan tanda backslash
         System.out.println("Lokasi file  : C:\\Java\\Belajar");
 
-        // Escape Sequence \'
+        // Escape Sequence \' = menampilkan tanda petik satu
         System.out.println("Karakter     : \'A\'");
     }
-}
