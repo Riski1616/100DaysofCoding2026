0720b294f4a4f6964a050808565f51e70728805f
Riski1616 <wwa725430@gmail.com>
2026-09-04 11:53:12 +0800

    Create Day03.java

---
+++ Day03.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Escape Sequence \n
+        System.out.println("Nama         : Riski\nKelas        : B");
+
+        // Escape Sequence \t
+        System.out.println("Nim\t     : D0226512");
+        System.out.println("Prodi\t     : Informatika");
+        System.out.println("alamat\t     : Majene");
+
+        // Escape Sequence \"
+        System.out.println("Saya berkata : \"Saya sedang belajar Java\"");
+
+        // Escape Sequence \\
+        System.out.println("Lokasi file  : C:\\Java\\Belajar");
+
+        // Escape Sequence \'
+        System.out.println("Karakter     : \'A\'");
+    }
+}
