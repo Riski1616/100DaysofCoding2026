b5e55f3e3381ccd949d91a2e6d2e001c30a1f378
Riski1616 <wwa725430@gmail.com>
2026-09-05 13:13:19 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Tipe data byte: untuk menyimpan bilangan bulat kecil
+        byte umur = 18;
+
+        // Tipe data short: untuk menyimpan bilangan bulat dengan nilai lebih besar dari byte
+        short tahun = 2008;
+
+        // Tipe data int: untuk menyimpan bilangan bulat
+        int nim = 90026512;
+
+        // Tipe data long: untuk menyimpan bilangan bulat dengan nilai yang sangat besar
+        long jumlahMahasiswa = 20000L;
+
+        System.out.println("umur            :" + umur);
+        System.out.println("tahun           :" + tahun);
+        System.out.println("nim             :" + nim);
+        System.out.println("jumlahMahasiswa :" + jumlahMahasiswa);
+    }
+}
