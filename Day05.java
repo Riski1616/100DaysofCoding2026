6637ae636cc02b0fc33c7aca6230f8e0ef2134f1
Riski1616 <wwa725430@gmail.com>
2026-09-06 12:57:29 +0800

    Create Day05.java

---
+++ Day05.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Tipe data float → digunakan untuk menyimpan bilangan pecahan
+        float tinggi = 155.5f; 
+
+        // Tipe data double → digunakan untuk menyimpan bilangan pecahan dengan lebih banyak angka desimal
+        double berat = 60.25; 
+
+        System.out.println("Tinggi : " + tinggi);
+        System.out.println("Berat  : " + berat);
+    }
+}
