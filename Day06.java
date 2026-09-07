ff9e4362a12b9f22d1c20e8301aae43574245c67
Riski1616 <wwa725430@gmail.com>
2026-09-07 18:39:04 +0800

    Create Day06.java

---
+++ Day06.java
@@ -0,0 +1,14 @@
+public class Main {           
+    public static void main(String[] args){
+
+        char jenisKelamin = 'p';
+       // char untuk menyimpan 1 karakter
+
+       
+        boolean mahasiswa = true;    
+        // boolean untuk nilai true atau false
+
+        System.out.println("Jenis Kelamin : " + jenisKelamin); 
+        System.out.println("Mahasiswa     : " + mahasiswa);
+    }                                        
+}                            
