3e7de2eca2cf357c12f013073f5e79c8297ef49b
Riski1616 <wwa725430@gmail.com>
2026-09-08 18:01:16 +0800

    Create Day07.java

---
+++ Day07.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+         // String adalah tipe data untuk menyimpan teks atau kumpulan karakter
+        String nama = "Riski";
+        String kelas = "B";
+        String prodi = "Informatika";
+
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("Prodi : " + prodi);
+    }
+}
