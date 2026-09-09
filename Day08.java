2eea42ebf441fce305bd741cbbdda22c88cd4d93
Riski1616 <wwa725430@gmail.com>
2026-09-09 18:27:12 +0800

    Create Day08.java

---
+++ Day08.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int umur; // Deklarasi: membuat variabel umur bertipe int
+
+        umur = 18; // Inisialisasi: memberikan nilai awal 18
+        System.out.println("sebelum diubah : " + umur); // Menampilkan nilai umur, yaitu 18
+
+        umur = 19; // Update: mengubah nilai umur dari 18 menjadi 19
+        System.out.println("sesudah diubah : "+ umur); // Menampilkan nilai umur, yaitu 19
+    }
+}
