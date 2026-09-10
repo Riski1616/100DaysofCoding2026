5befce5a2c024f1df452d181467efe87281e1bf5
Riski1616 <wwa725430@gmail.com>
2026-09-10 16:19:39 +0800

    Create Day09.java

---
+++ Day09.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        final double PI = 3.14; // Final/konstanta: nilai PI tidak dapat diubah
+
+        System.out.println("Nilai PI: " + PI); 
+    }
+}
