483e0b6ca8acf93e642192eb12d20bdfd6624843
Riski1616 <wwa725430@gmail.com>
2026-09-15 20:05:15 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int angka1 = 20;
+        int angka2 = 5;
+
+        // Penjumlahan
+        int hasilTambah = angka1 + angka2;
+
+        // Pengurangan
+        int hasilKurang = angka1 - angka2;
+
+        System.out.println("Angka 1 = " + angka1);
+        System.out.println("Angka 2 = " + angka2);
+
+        System.out.println("Hasil Penjumlahan = " + hasilTambah);
+        System.out.println("Hasil Pengurangan = " + hasilKurang);
+    }
+}
