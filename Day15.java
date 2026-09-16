a72fe5886610e3e55c2c5124adfcb2d75c75f0dc
Riski1616 <wwa725430@gmail.com>
2026-09-16 20:47:32 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 10;
+
+        // Perkalian
+        int hasilPerkalian = a * b;
+
+        // Pembagian
+        int hasilPembagian = a / b;
+
+        System.out.println("Hasil Perkalian = " + hasilPerkalian);
+        System.out.println("Hasil Pembagian = " + hasilPembagian);
+    }
+}
