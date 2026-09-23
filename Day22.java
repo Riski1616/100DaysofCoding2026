e359d6cefe74ac2eeeec706cca3e4328734d3533
Riski1616 <wwa725430@gmail.com>
2026-09-23 19:22:37 +0800

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Day22 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Memasukkan nilai pertama
+        System.out.print("Masukkan nilai A: ");
+        int A = input.nextInt();
+
+        // Memasukkan nilai kedua
+        System.out.print("Masukkan nilai B: ");
+        int B = input.nextInt();
+
+        System.out.println("\nSebelum ditukar:");
+        System.out.println("A = " + A);
+        System.out.println("B = " + B);
+
+        // Menukar nilai menggunakan variabel sementara
+        int temp = A;
+        A = B;
+        B = temp;
+
+        System.out.println("\nSetelah ditukar:");
+        System.out.println("A = " + A);
+        System.out.println("B = " + B);
+    }
+}
